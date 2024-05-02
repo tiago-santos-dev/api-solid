b33158e0f938cb68382b42592114933fd3be3014
Tiago Santos <trodrigues322@gmail.com>
2024-05-02 06:40:23 -0300

    feat: :sparkles: authenticate

---
+++ src/use-cases/authenticate.spec.ts
@@ -0,0 +1,30 @@
+import { InMemoryUsersRepository } from '@/repositories/in-memory/in-memory-users-repository'
+import { hash } from 'bcryptjs'
+import { beforeEach, describe, expect, it } from 'vitest'
+import { AuthenticateUseCase } from './authenticate'
+
+let usersRepository: InMemoryUsersRepository
+let sut: AuthenticateUseCase
+
+describe('Authenticate Use Case', () => {
+    beforeEach(() => {
+        usersRepository = new InMemoryUsersRepository()
+        sut = new AuthenticateUseCase(usersRepository)
+    })
+
+    it('should be able to authenticate', async () => {
+
+        await usersRepository.create({
+            name: 'John Doe',
+            email: 'johndoe@example.com',
+            password_hash: await hash('123456', 6),
+        })
+
+        const { user } = await sut.execute({
+            email: 'johndoe@example.com',
+            password: '123456',
+        })
+
+        expect(user.id).toEqual(expect.any(String))
+    })
+})
