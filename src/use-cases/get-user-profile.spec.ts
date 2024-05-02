50a3e2655aca532664b4e06e6b7bac956775c35e
Tiago Santos <trodrigues322@gmail.com>
2024-05-02 07:59:50 -0300

    test: :white_check_mark: get user profile

---
+++ src/use-cases/get-user-profile.spec.ts
@@ -0,0 +1,38 @@
+import { InMemoryUsersRepository } from '@/repositories/in-memory/in-memory-users-repository'
+import { hash } from 'bcryptjs'
+import { beforeEach, describe, expect, it } from 'vitest'
+import { ResourceNotFoundError } from './errors/resource-not-found'
+import { GetUserProfileUseCase } from './get-users-profile'
+
+let usersRepository: InMemoryUsersRepository
+let sut: GetUserProfileUseCase
+
+describe('Get User Profile Use Case', () => {
+    beforeEach(() => {
+        usersRepository = new InMemoryUsersRepository()
+        sut = new GetUserProfileUseCase(usersRepository)
+    })
+
+    it('should be able to get user profile', async () => {
+
+        const createUser = await usersRepository.create({
+            name: 'John Doe',
+            email: 'johndoe@example.com',
+            password_hash: await hash('123456', 6),
+        })
+
+        const { user } = await sut.execute({
+            userId: createUser.id,
+        })
+
+        expect(user.name).toEqual('John Doe')
+    })
+
+    it('should not be able to get user profile with wrong id', async () => {
+        await expect(() =>
+            sut.execute({
+                userId: 'non-existing-id',
+            }),
+        ).rejects.toBeInstanceOf(ResourceNotFoundError)
+    })
+})
